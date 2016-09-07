e1819ab0680f269d31a3ad4920f0d1532d9d2bf0
KaidinJackson <jacksonkaidin@gmail.com>
2016-09-07 15:19:15 +1000

    Add files via upload

---
+++ WP2/Assignment2/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<!-- meta http-equiv="X-Frame-Options" content="deny" -->
+<title>Home | Josh Thomas</title>
+<!-- We will cover style later. Skip down to the body tag -->
+<style>
+html, body {
+	margin: 0px;
+	padding: 0px;
+}
+header {
+	font-size: 300%;
+	font-weight: bold;
+	text-align: center;
+	font-family: Consolas, "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", Monaco, "Courier New", monospace;
+	background-image: url("resources/jt.png");
+	background-repeat: no-repeat;
+	background-size:contain;
+	background-color: rgb(173,215,208);
+	padding: 50px;
+}
+header * {
+	vertical-align: middle;
+	margin-right: 10px;
+	margin-left: 10px;
+	text-align: center
+}
+header img {
+	float: right;
+	text-align: center
+}
+main {
+	background: white;
+	padding-top: 30px;
+	padding-right: 100px;
+	padding-left: 100px;
+	padding-bottom: 30px;
+	font-family: Consolas, "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", Monaco, "Courier New", monospace;
+	text-align: center;
+	text-shadow: 0px 0px;
+}
+nav, footer {
+	background-color: #F2CECE;
+	padding: 20px;
+	font-family: Segoe, "Segoe UI", "DejaVu Sans", "Trebuchet MS", Verdana, sans-serif;
+	font-weight: bold;
+	font-colour: white;
+	color: #FFFFFF;
+	text-align: center
+}
+footer * {
+	vertical-align: middle;
+}
+</style>
+</head>
+
+
+<body>
+
+<header> 
+<div>
+<a href="https://www.facebook.com/officialjoshthomas">
+  <img src="resources/fb.png" alt='FB' style="width:42px;height:42px;border:0;">
+</a>
+<a href="http://twitter.com/joshthomas87">
+  <img src="resources/twit.png" alt="TWIT" style="width:42px;height:42px;border:0;">
+</a>
+<a href="http://instagram.com/joshthomas87/">
+  <img src="resources/insta.png" alt="INSTA" style="width:42px;height:42px;border:0;">
+</a>
+</div>
+
+<a href="index.php"  style="text-decoration:none;"><font color="#F2CECE">Josh Thomas</font></a>  
+
+</header>
+
+<nav> <a href="index.php" style="text-decoration:underline;"><font color="white">Home</font></a> | <a href="contact.php" style="text-decoration:none;"><font color="white">Contact</font></a> | <a href="shop.php" style="text-decoration:none;"><font color="white">Shop</font></a> </nav>
+<main>
+
+
+
+<div>
+ <iframe width="500" height="300" src="https://www.youtube.com/embed/5dgWP7NHByY?autoplay=1" frameborder="0" allowfullscreen></iframe>
+</div>
+
+<p> <a href="header"  style="text-decoration:none;"><font color="#ED8C8E">After making his mark as an award-winning and hugely-successful stand-up comedian and television panellist, Josh Thomas turned his hand to writing, acting and co-executive producing his own television show for ABC TV, inspired by his life and his comedy, called Please Like Me.</font></a> </p>
+
+
+
+</main>
+
+
+
+<footer> &copy; Test
+  <script>
+        document.write(new Date().getFullYear());
+      </script>
+  ,
+  Test </footer>
+</body>
+</html>
---
+++ WP2/Assignment2/shop.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<!-- meta http-equiv="X-Frame-Options" content="deny" -->
+<title>Official | Josh Thomas</title>
+<!-- We will cover style later. Skip down to the body tag -->
+<style>
+html, body {
+	margin: 0px;
+	padding: 0px;
+	color: black;
+	text-align: center
+}
+header {
+	font-size: 300%;
+	font-weight: lighter;
+	font-family: Consolas, "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", Monaco, "Courier New", monospace;
+	background: white;
+	padding: 50px;
+ text-align: center background: url("test2.png");
+	position: static;
+}
+header * {
+	vertical-align: middle;
+	margin-right: 10px;
+	margin-left: 10px;
+	text-align: center
+}
+header img {
+	float: right;
+	text-align: center
+}
+main {
+	background: white;
+	padding: 0;
+	font-family: Helvetica;
+	text-align: center;
+	text-shadow: 0px 0px;
+}
+nav, footer {
+	background-color: rgba(242,206,206,1.00);
+	padding: 20px;
+	font-family: Segoe, "Segoe UI", "DejaVu Sans", "Trebuchet MS", Verdana, sans-serif;
+	font-weight: bold;
+	font-colour: white;
+	color: #FFFFFF;
+	text-align: center
+}
+footer * {
+	vertical-align: middle;
+}
+#aboutme {
+	border-collapse: collapse;
+	border: 0px solid #333;
+	width: 100%;
+	background-color: rgb(214,214,214);
+}
+#aboutme th {
+	background: gray;
+	text-align: left;
+	font-family: Helvetica;
+	width: 180px;
+}
+#aboutme th, #aboutme td {
+	border: 0px solid #FFEBEC;
+	padding: 10px;
+	font-family: Helvetica;
+	font-size: 80%;
+}
+</style>
+</head>
+
+<body>
+<header> 
+<div>
+<a href="https://www.facebook.com/officialjoshthomas">
+  <img src="fb.png" alt='FB' style="width:42px;height:42px;border:0;">
+</a>
+<a href="http://twitter.com/joshthomas87">
+  <img src="twit.png" alt="TWIT" style="width:42px;height:42px;border:0;">
+</a>
+<a href="http://instagram.com/joshthomas87/">
+  <img src="insta.png" alt="INSTA" style="width:42px;height:42px;border:0;">
+</a>
+</div>
+
+<a href="index.php"  style="text-decoration:none;"><font color="#ED8C8E">Josh Thomas</font></a> 
+
+</header>
+<nav> <a href="index.php" style="text-decoration:none;"><font color="white">Home</font></a> | <a href="contact.php"  style="text-decoration:none;"><font color="white">Contact</font></a> | <a href="shop.php" style="text-decoration:underline;"><font color="white">Shop</font></a> </nav>
+<main>
+<p> <a href="header"  style="text-decoration:none;"><font color="#ED8C8E">SHOP</font></a> </p>
+
+				
+
+
+
+</main>
+<footer> &copy; Test
+  <script>
+        document.write(new Date().getFullYear());
+      </script>
+  ,
+  Test </footer>
+</body>
+</html>
